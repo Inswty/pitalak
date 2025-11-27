63085cee86c7f2a88d12df9498b8f9faa807dfdd
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2025-11-27 16:15:09 +0500

    feat(database): add support for PostgreSQL and SQLite configurations

--- backend/pitalak_backend/settings.py
+++ backend/pitalak_backend/settings.py
@@ -93,13 +93,25 @@ CACHES = {
 }
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+USE_SQLITE = os.getenv('USE_SQLITE', 'False') == 'True'
+if USE_SQLITE:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', 5432),
+        }
+    }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
