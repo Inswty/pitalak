e184f87122fbc9fe718feab4eba7779924d0889a
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2025-12-09 13:11:56 +0500

    fix(bot_telegram): truncate error message for Telegram sending

--- backend/core/bot_telegram_logger.py
+++ backend/core/bot_telegram_logger.py
@@ -1,6 +1,9 @@
 import logging
+from textwrap import shorten
 
 from .tasks import send_log_to_telegram
+
+MAX_TG_MESSAGE_LENGTH = 300  # Лимит симоолов telegram сообщения в log
 
 
 class TelegramHandler(logging.Handler):
@@ -9,6 +12,9 @@ class TelegramHandler(logging.Handler):
     def emit(self, record):
         try:
             msg = self.format(record)
+            msg = shorten(
+                msg, width=MAX_TG_MESSAGE_LENGTH, placeholder='\n…(truncated)'
+            )
             send_log_to_telegram.delay(msg)
         except Exception:
             pass
