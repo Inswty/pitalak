5cd3e4475cc3245fb8455c20cba1b044d6a905dd
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2026-01-10 23:54:43 +0500

    feat(order): add Delivery, PayMethod models

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -266,3 +266,20 @@ class DeliveryRule(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PayMethod(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Delivery(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
