4007ae50204618562a713c662fe50b97258a3310
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2026-01-25 11:51:44 +0500

    feat(order): add create_order_for_checkout service method

--- backend/orders/services.py
+++ backend/orders/services.py
@@ -3,7 +3,9 @@ from decimal import Decimal
 
 from django.db import transaction
 
-from .models import DeliveryRule, Order, OrderItem
+from .models import (
+    DeliveryRule, Order, OrderItem, Payment, ShoppingCart,
+)
 
 
 class OrderService:
@@ -11,20 +13,27 @@ class OrderService:
 
     @classmethod
     @transaction.atomic
-    def create_from_cart(cls, cart):
+    def create_from_cart(cls, cart, *, order_data=None):
         """Создаёт новый заказ на основе корзины пользователя."""
         if not cart.items.exists():
             raise ValueError('Невозможно создать заказ из пустой корзины.')
-        # Используем адрес из корзины
-        if not cart.address:
-            raise ValueError('В корзине не выбран адрес. '
-                             'Добавьте адрес перед оформлением заказа.')
+
+        order_data = order_data or {}
+        # Получаем объект Delivery из order_data, если он передан
+        delivery = order_data.get('delivery')
+        delivery_price = delivery.price if delivery else Decimal('0.00')
+        items_total = sum(
+            item.product.price * item.quantity for item in cart.items.all()
+        )
+        total_price = items_total + delivery_price
         # Создаём заказ
         order = Order.objects.create(
             user=cart.user,
-            address=cart.address,
-            status='new',
-            total_price=Decimal('0.00'),
+            status=Order.Status.NEW,
+            delivery_price=delivery_price,
+            items_total=items_total,
+            total_price=total_price,
+            **order_data,
         )
         # Формируем список объектов OrderItem
         order_items = [
@@ -32,20 +41,39 @@ class OrderService:
                 order=order,
                 product=item.product,
                 quantity=item.quantity,
-                price=item.product.price,  # Актуальная цена
+                price=item.product.price,
             )
             for item in cart.items.select_related('product')
         ]
-
         # Сохраняем все позиции одним запросом
         OrderItem.objects.bulk_create(order_items)
-        # Пересчитываем сумму
-        order.total_price = sum(
-            item.price * item.quantity for item in order_items
-        )
-        order.save(update_fields=['total_price'])
         # Очищаем корзину
         cart.items.all().delete()
+        return order
+
+    @classmethod
+    def create_order_for_checkout(cls, user, validated_data):
+        """Создаёт заказ для оформления (checkout)."""
+        cart = ShoppingCart.objects.get(user=user)
+        order = cls.create_from_cart(
+            cart,
+            order_data={
+                'delivery': validated_data['delivery'],
+                'address': validated_data['address'],
+                'comment': validated_data.get('comment'),
+                'payment_method': validated_data.get('payment_method'),
+            }
+        )
+        # Создаём объект оплаты
+        Payment.objects.create(
+            order=order,
+            method=validated_data['payment_method'],
+            amount=order.total_price,
+            status=Payment.Status.PENDING
+        )
+        # Меняем статус заказа
+        order.status = Order.Status.PROCESSING
+        order.save(update_fields=['status'])
         return order
 
     @classmethod
@@ -55,16 +83,13 @@ class OrderService:
         активных правил и времени создания заказа.
         """
         rules = DeliveryRule.objects.filter(is_active=True)
-
         slots = []
         order_time = order_created_at.time()
-
         for rule in rules:
             if rule.time_from <= order_time <= rule.time_to:
                 date = (
                     order_created_at + timedelta(days=rule.days_offset)
                 ).date()
-
                 slots.append({
                     'date': date,
                     'time_from': rule.delivery_time_from,
@@ -73,5 +98,4 @@ class OrderService:
                                f'{rule.delivery_time_from.strftime('%H:%M')}-'
                                f'{rule.delivery_time_to.strftime('%H:%M')}'
                 })
-
         return slots
